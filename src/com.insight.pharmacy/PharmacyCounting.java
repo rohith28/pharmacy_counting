8435dad9d589dd879ce866419edc7923f384626c
rohith28 <rohith28@github.com>
2018-07-15 07:02:58 -0600

    Updated reading and writing

--- src/com.insight.pharmacy/PharmacyCounting.java
+++ src/com.insight.pharmacy/PharmacyCounting.java
@@ -3,18 +3,20 @@ package com.insight.pharmacy;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.util.*;
 
 public class PharmacyCounting{
 
     public static void main(String[] args) throws IOException {
         FileInputStream inputStream = null;
         Scanner sc = null;
-
-        Map<String,int[]> hMap = new HashMap<String,int[]>();
-
+        List<drugDetails> listOfPrint = new ArrayList<>();
+        Set<String> names = new HashSet<>();
         try {
             String path = "input/itcont.txt";
             inputStream = new FileInputStream(path);
@@ -23,47 +25,62 @@ public class PharmacyCounting{
 
 
             while (sc.hasNextLine()) {
-                if (header == 0) {
+
+                if(header == 0){
                     sc.nextLine();
                     header = 1;
                 }
-                else{
-                String line = sc.nextLine();
-                String[] arr = line.split(",");
 
-                     if (hMap.containsKey(arr[3])) {
+                else {
+                    String line = sc.nextLine();
+                    String[] arr = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                    arr[3] = arr[3].replace("\"","");
+                    drugDetails reomvePrint = null;
+                    if (names.contains(arr[3])) {
 
-                        int[] temp = hMap.get(arr[3]);
-                        temp[1] = Integer.parseInt(arr[4]) + temp[1];
-                        temp[0] = temp[0] + 1;
-                         hMap.put(arr[3],temp);
+                        drugDetails updatePrint = new drugDetails(arr[3],0,0);
+                        Iterator<drugDetails> itr  = listOfPrint.iterator();
+                        while(itr.hasNext()){
+                            drugDetails temp = itr.next();
+                            if(temp.getdName().equals(arr[3])){
+                                reomvePrint = temp;
+                            }
+                        }
+
+                        for (drugDetails temp : listOfPrint) {
+                            if(temp.getdName().equals(arr[3])){
+                                updatePrint.setNum(temp.getNum() + 1);
+                                updatePrint.setCost(temp.getCost() + Float.parseFloat(arr[4]));
+                            }
+
+                       }
+
+
+                       listOfPrint.remove(reomvePrint);
+                       listOfPrint.add(updatePrint);
+
 
                     } else {
-                        int[] temp = new int[2];
-                        temp[1] = Integer.parseInt(arr[4]);
-                        temp[0] = 1;
-                        hMap.put(arr[3],temp);
+                        try {
+                            drugDetails tempPrint = new drugDetails(arr[3], 1, Float.parseFloat(arr[4]));
+                            listOfPrint.add(tempPrint);
+                            names.add(arr[3]);
+                        }catch (NumberFormatException ne){
+                            System.out.println(ne.getMessage());
 
-
-                    }
-
-
-
-             }
-
-
-            }
-
-            for(Map.Entry entry : hMap.entrySet()){
-                int[] integers = hMap.get(entry.getKey());
-                System.out.print(entry.getKey()+" ");
-                for(int i=0;i<integers.length;i++){
-                    System.out.print(integers[i]+" ");
+                            System.out.println("----"+arr[4]+" ----"+arr[0]);
+                        }
+                        }
                 }
-
-                System.out.println  ();
-
             }
+
+
+            Collections.sort(listOfPrint,new SortBycost());
+            Path file = Paths.get("output/top-cost-drug.txt");
+            for(drugDetails tempList : listOfPrint){
+                System.out.println(tempList.toString());
+            }
+
             // note that Scanner suppresses exceptions
             if (sc.ioException() != null) {
                 throw sc.ioException();
@@ -72,12 +89,11 @@ public class PharmacyCounting{
             System.out.println(fe.getMessage());
         }
 
-
         finally {
             if (inputStream != null) {
                 inputStream.close();
             }
-            if (sc != null) {
+            if (sc != null) {   
                 sc.close();
             }
         }
