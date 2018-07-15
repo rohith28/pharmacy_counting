897c3bafb431170df0930fb32a4bc7949ca59f0a
rohith28 <rohith28@github.com>
2018-07-15 16:34:36 -0600

    Modular code

--- src/com.insight.pharmacy/PharmacyCounting.java
+++ src/com.insight.pharmacy/PharmacyCounting.java
@@ -1,102 +1,116 @@
 package com.insight.pharmacy;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.KeyStore;
+import java.io.*;
 import java.util.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PharmacyCounting{
+public class PharmacyCounting {
 
     public static void main(String[] args) throws IOException {
+
+        List<drugDetails> listOfPrint = new ArrayList<>();
+
+            String path = "input/input.txt";
+
+            listOfPrint = readDataFromFile(path, listOfPrint);
+
+            Collections.sort(listOfPrint, new SortBycost());
+
+            int count = 0;
+            //Path file = Paths.get("output/top-cost-drug.txt");
+
+            FileWriter fileWriter = new FileWriter("output/top_cost_drug.txt");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.print("drug_name,num_prescriber,total_cost\n");
+
+
+            for (drugDetails tempList : listOfPrint) {
+                count++;
+                String str = tempList.getdName() + "," + tempList.getNum() + "," + tempList.getCost() + "\n";
+                printWriter.print(str);
+
+            }
+            System.out.print(count);
+            printWriter.close();
+            // note that Scanner suppresses exceptions
+
+
+    }
+
+
+    public static List<drugDetails> readDataFromFile(String path, List<drugDetails> listOfPrint) throws IOException{
         FileInputStream inputStream = null;
         Scanner sc = null;
-        List<drugDetails> listOfPrint = new ArrayList<>();
         Set<String> names = new HashSet<>();
+
         try {
-            String path = "input/itcont.txt";
+
             inputStream = new FileInputStream(path);
+
             sc = new Scanner(inputStream, "UTF-8");
             int header = 0;
 
-
             while (sc.hasNextLine()) {
 
-                if(header == 0){
+                if (header == 0) {
                     sc.nextLine();
                     header = 1;
-                }
-
-                else {
+                } else {
                     String line = sc.nextLine();
                     String[] arr = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-                    arr[3] = arr[3].replace("\"","");
+                    arr[3] = arr[3].replace("\"", "");
                     drugDetails reomvePrint = null;
                     if (names.contains(arr[3])) {
 
-                        drugDetails updatePrint = new drugDetails(arr[3],0,0);
-                        Iterator<drugDetails> itr  = listOfPrint.iterator();
-                        while(itr.hasNext()){
+                        drugDetails updatePrint = new drugDetails(arr[3], 0, 0);
+                        Iterator<drugDetails> itr = listOfPrint.iterator();
+                        while (itr.hasNext()) {
                             drugDetails temp = itr.next();
-                            if(temp.getdName().equals(arr[3])){
+                            if (temp.getdName().equals(arr[3])) {
                                 reomvePrint = temp;
                             }
                         }
+                        try {
+                            for (drugDetails temp : listOfPrint) {
+                                if (temp.getdName().equals(arr[3])) {
+                                    updatePrint.setNum(temp.getNum() + 1);
+                                    updatePrint.setCost(temp.getCost() + Float.parseFloat(arr[4]));
+                                }
 
-                        for (drugDetails temp : listOfPrint) {
-                            if(temp.getdName().equals(arr[3])){
-                                updatePrint.setNum(temp.getNum() + 1);
-                                updatePrint.setCost(temp.getCost() + Float.parseFloat(arr[4]));
                             }
+                        } catch (NumberFormatException ne) {
+                            System.out.println();
+                        }
 
-                       }
-
-
-                       listOfPrint.remove(reomvePrint);
-                       listOfPrint.add(updatePrint);
-
-
+                        listOfPrint.remove(reomvePrint);
+                        listOfPrint.add(updatePrint);
                     } else {
                         try {
                             drugDetails tempPrint = new drugDetails(arr[3], 1, Float.parseFloat(arr[4]));
                             listOfPrint.add(tempPrint);
                             names.add(arr[3]);
-                        }catch (NumberFormatException ne){
+                        } catch (NumberFormatException ne) {
                             System.out.println(ne.getMessage());
-
-                            System.out.println("----"+arr[4]+" ----"+arr[0]);
                         }
-                        }
+                    }
                 }
             }
-
-
-            Collections.sort(listOfPrint,new SortBycost());
-            Path file = Paths.get("output/top-cost-drug.txt");
-            for(drugDetails tempList : listOfPrint){
-                System.out.println(tempList.toString());
-            }
-
-            // note that Scanner suppresses exceptions
-            if (sc.ioException() != null) {
-                throw sc.ioException();
-            }
-        }catch (FileNotFoundException fe){
+        } catch (FileNotFoundException fe) {
+            System.out.println("File not found. Please enter correct path.");
             System.out.println(fe.getMessage());
-        }
-
-        finally {
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        } finally {
             if (inputStream != null) {
-                inputStream.close();
+                 inputStream.close();
             }
-            if (sc != null) {   
+            if (sc != null) {
                 sc.close();
             }
         }
-    }
 
+        return listOfPrint;
+    }
 }
